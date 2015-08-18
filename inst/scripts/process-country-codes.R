c1ddec8f1c6e7ff291adb25f796461f8b2017aea
Toph Allen <toph.allen@gmail.com>
2015-08-18 11:24:22 -0400

    Cleaned up country code processing script a bit.

--- inst/scripts/process-country-codes.R
+++ inst/scripts/process-country-codes.R
@@ -1,16 +1,10 @@
-load_all()
-
-library(dplyr)
-
 country_codes = read.csv("inst/rawdata/FAOCountryCodes.csv", stringsAsFactors = FALSE)
-
 names(country_codes) <- c("shortname",
-                      "officialname",
-                      "iso3",
-                      "iso2",
-                      "uni",
-                      "undp",
-                      "faostat",
-                      "gaul")
-
+                          "officialname",
+                          "iso3",
+                          "iso2",
+                          "uni",
+                          "undp",
+                          "faostat",
+                          "gaul")
 save(country_codes, file = "data/country_codes.RData")
